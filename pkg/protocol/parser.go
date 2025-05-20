616b82f4aa8fb93cfd78ad8415625612be3240af
soroush safari <soroush.safari1992@gmail.com>
2025-05-20 13:48:11 +0330

    delete checksum

--- pkg/protocol/parser.go
+++ pkg/protocol/parser.go
@@ -18,9 +18,8 @@ const (
 )
 
 type Header struct {
-	Opcode   byte
-	Length   uint32
-	CheckSum uint32
+	Opcode byte
+	Length uint32
 }
 type Message struct {
 	Header Header
@@ -36,25 +35,20 @@ func calculateChecksum(data []byte) uint32 {
 }
 
 func ParseMessage(conn net.Conn) (*Message, error) {
-	headerBuf := make([]byte, 9)
+	headerBuf := make([]byte, 5)
 
 	if _, err := io.ReadFull(conn, headerBuf); err != nil {
 		return nil, fmt.Errorf("read header failed: %w", err)
 	}
 
 	header := Header{
-		Opcode:   headerBuf[0],
-		Length:   binary.BigEndian.Uint32(headerBuf[1:5]),
-		CheckSum: binary.BigEndian.Uint32(headerBuf[5:9]),
+		Opcode: headerBuf[0],
+		Length: binary.BigEndian.Uint32(headerBuf[1:5]),
 	}
 
 	body := make([]byte, header.Length)
 	if _, err := io.ReadFull(conn, body); err != nil {
 		return nil, fmt.Errorf("read body failed: %w", err)
-	}
-
-	if calc := calculateChecksum(body); calc != header.CheckSum {
-		return nil, fmt.Errorf("checksum mismatch: expected %d, got %d", header.CheckSum, calc)
 	}
 
 	return &Message{
@@ -65,13 +59,11 @@ func ParseMessage(conn net.Conn) (*Message, error) {
 
 func EncodeMessage(msg *Message) ([]byte, error) {
 	msg.Header.Length = uint32(len(msg.Body))
-	msg.Header.CheckSum = calculateChecksum(msg.Body)
 
-	buf := make([]byte, 9+len(msg.Body))
+	buf := make([]byte, 5+len(msg.Body))
 	buf[0] = msg.Header.Opcode
 	binary.BigEndian.PutUint32(buf[1:5], msg.Header.Length)
-	binary.BigEndian.PutUint32(buf[5:9], msg.Header.CheckSum)
-	copy(buf[9:], msg.Body)
+	copy(buf[5:], msg.Body)
 
 	return buf, nil
 }
