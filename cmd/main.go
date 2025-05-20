fc28f0bb485f934ee48d067b3d53c909092503dd
soroush safari <soroush.safari1992@gmail.com>
2025-05-20 12:09:36 +0330

    add tco server for main

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"github.com/coci/chitchat/pkg/protocol"
+	"net"
+)
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	for {
+		msg, err := protocol.ParseMessage(conn)
+		if err != nil {
+			fmt.Println("Parse error:", err)
+			return
+		}
+		fmt.Printf("Received: Opcode=0x%02X, Length=%d, Body=%s\n",
+			msg.Header.Opcode, msg.Header.Length, string(msg.Body))
+	}
+}
+
 func main() {
-	
+	ln, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Server listening on :9000")
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Accept error:", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
 }
