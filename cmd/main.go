77fcdc666bc61653177991e8bbcaee79946d8850
soroush <soroush.safari1992@gmail.com>
2025-10-27 22:46:03 +0330

    Integrate `ZapLogger` into `gateway.Server` for enhanced structured logging.

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"chitchat/pkg/gateway"
+	"chitchat/pkg/logger"
 	"chitchat/pkg/protocol"
 )
 
 func main() {
-	newServer := gateway.NewServer(protocol.Gossip{})
+	newServer := gateway.NewServer(protocol.Gossip{}, logger.NewZapLogger())
 
 	newServer.Serve()
 }
