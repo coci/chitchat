433f8b4f143bf2598cbde634668bb029866d2929
soroush <soroush.safari1992@gmail.com>
2025-10-27 22:10:42 +0330

    Refactor `interface.go` to simplify definitions by replacing `Frame` struct with `IFrame` interface and update `IProtocol` methods accordingly.

--- pkg/protocol/interface.go
+++ pkg/protocol/interface.go
@@ -1,27 +1,8 @@
 package protocol
 
-type Header struct {
-	Magic       uint16
-	Version     uint8
-	MessageType uint8
-	StreamId    uint32
-	Length      uint16
-}
-
-type LongTail struct {
-	SessionId uint32
-	Sequence  uint64
-	Tag       uint64
-}
-
-type Frame struct {
-	Header   Header
-	LongTail *LongTail
-	Body     []byte
-}
+type IFrame interface{}
 
 type IProtocol interface {
-	ParseMessage(data []byte) (Frame, error)
-	SerializeMessage(Frame) []byte
-	FrameToString(Frame) string
+	ParseMessage(data []byte) (IFrame, error)
+	SerializeMessage(IFrame) []byte
 }
